cdc3693b4c622502873d844ddf7521698703f4cc
Jonathan Di Rico <59540565+yonasuriv@users.noreply.github.com>
2022-06-20 02:35:29 +0200

    Uploaded JSC from CodePen

---
+++ script.js
@@ -0,0 +1,36 @@
+const consoleInput = document.querySelector(".console-input");
+const historyContainer = document.querySelector(".console-history");
+
+function addResult(inputAsString, output) {
+  const outputAsString =
+    output instanceof Array ? `[${output.join(", ")}]` : output.toString();
+  const inputLogElement = document.createElement("div");
+  const outputLogElement = document.createElement("div");
+
+  inputLogElement.classList.add("console-input-log");
+  outputLogElement.classList.add("console-output-log");
+
+  inputLogElement.textContent = `> ${inputAsString}`;
+  outputLogElement.textContent = outputAsString;
+
+  historyContainer.append(inputLogElement, outputLogElement);
+}
+
+consoleInput.addEventListener("keyup", (e) => {
+  const code = consoleInput.value.trim();
+
+  if (code.length === 0) {
+    return;
+  }
+
+  if (e.key === "Enter") {
+    try {
+      addResult(code, eval(code));
+    } catch (err) {
+      addResult(code, err);
+    }
+
+    consoleInput.value = "";
+    historyContainer.scrollTop = historyContainer.scrollHeight;
+  }
+});
